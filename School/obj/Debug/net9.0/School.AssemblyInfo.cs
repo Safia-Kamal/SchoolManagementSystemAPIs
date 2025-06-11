7ba2ef383c7a12dc306e950c34a1a31f77b43607
Safiya Kamal <safiakamal552@gmail.com>
2025-06-11 18:41:10 +0300

    first commit

--- School/obj/Debug/net9.0/School.AssemblyInfo.cs
+++ School/obj/Debug/net9.0/School.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21a012fb24017844be9e65dedd4a50f232a19c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("School")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
